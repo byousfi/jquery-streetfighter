6bdf564385c442f46dc2af400a6dd5ad047607cb
byousfi <byousfi@gmail.com>
2015-10-20 14:47:16 -0700

    css/main.css

--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
       $('.ryu-still').hide();
       $('.ryu-ready').show();
   })
+
   .mouseleave(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
   })
+
   .mousedown(function() {
     playHadouken();
     $('.ryu-ready').hide();
@@ -15,13 +17,26 @@ $(document).ready(function() {
       {'left': '1020px'},500, function() {
     $(this).hide();
     $(this).css('left','520px');
-  }
-);
+  });
   })
+
   .mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
+})
+
+$("html").keydown(function (e){
+  if (e.keyCode == 88) {
+    $(".ryu-ready").hide();
+    $(".ryu-cool").show();
+  }
+});
+$("html").keyup(function (e){
+  if (e.keyCode == 88) {
+    $(".ryu-ready").show();
+    $(".ryu-cool").hide();
+  }
 });
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
